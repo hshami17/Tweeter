859fd7be34831c7f94ca2c5419e72353d3fb53c4
hshami17 <hasanshami2013@gmail.com>
2015-11-15 16:44:44 -0500

    Added tagged post form and made some changes to explore mode

--- Group Project/src/group/project/frmLogin.java
+++ Group Project/src/group/project/frmLogin.java
@@ -65,7 +65,7 @@ public class frmLogin extends Application {
 
         // Name text field
         txtUsername = new TextField();
-        //txtUsername.setText("hshami");
+        txtUsername.setText("hshami");
         txtUsername.setFont(Font.font("Helvetica", 18));
         GridPane.setConstraints(txtUsername, 1, 1);
         GridPane.setColumnSpan(txtUsername, 2);
@@ -89,7 +89,7 @@ public class frmLogin extends Application {
 
         // Password text field
         txtPassword = new PasswordField();
-        //txtPassword.setText("toilet");
+        txtPassword.setText("toilet");
         txtPassword.setFont(Font.font("Helvetica", 18));
         GridPane.setConstraints(txtPassword, 1, 2);
         GridPane.setColumnSpan(txtPassword, 2);
@@ -117,6 +117,7 @@ public class frmLogin extends Application {
                     // Get the username from text field and get current user likes from file
                     Profile.username = txtUsername.getText().trim();
                     Profile.retrieveLikes();
+                    Profile.retrieveTaggedPosts();
                     exploreMode = false;
                     frmHomePage.display();
                 } else
